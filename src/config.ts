eff4c9dcbbaedc566d9e1e14686cff1b79ac5f12
Otavio Ehrenberger <otamm@Otavios-MacBook-Pro.local>
2023-05-15 15:29:49 +0200

    added global vars

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,26 @@
-// export const API_URL: string = 'api.com;
-// export const DATABASE_URL = 'mysql';
+export const API_URL: string = 'https://api.artic.edu/api/v1/artworks' // /{id} for show, ?page={pageNum}&limit={itemsOnPage} for index
+export const DEFAULT_ARTWORK_FIELDS_FOR_INDEX: string = [
+    'id',
+    'title',
+    'artist_display',
+    'date_display',
+    'has_not_been_viewed_much'
+].join(',')
+
+// @ts-ignore
+export const DEFAULT_ARTWORK_FIELDS_FOR_SHOW: string = [
+    'id',
+    'title',
+    'artist_display',
+    'date_display',
+    'has_not_been_viewed_much',
+    'alt_titles',
+    'thumbnail',
+    'medium_display',
+    'exhibition_history'
+].join(',')
+export const DATABASE_URL = 'mysql';
+
 // using a placeholder value as a json web token is meant only for tests, this should never be in a production env as it allows for a hard-coded token.
+// @ts-ignore
 export const JWT_SECRET: string = process.env.JWT_SECRET || 'ssshhhhh,secret!!!';
