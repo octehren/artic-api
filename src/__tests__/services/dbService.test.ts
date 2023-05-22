32335656e79941f88bd551f597d53a09a085568d
octehren <108275018+octehren@users.noreply.github.com>
2023-05-22 18:28:35 +0200

    refactor + debugging

--- src/__tests__/services/dbService.test.ts
+++ src/__tests__/services/dbService.test.ts
@@ -1,24 +1,22 @@
 import mysql from 'mysql2/promise';
-import { createArtwork, getUserEmailByArtworkId } from '../../services/dbService';
-import { DEFAULT_POOL_CONFIG } from '../../config';
+import { pool, createArtwork, getUserEmailByArtworkId } from '../../services/dbService';
 
-let pool: mysql.Pool;
+
 let testUserId: number;
 const existingArtwork = 101;
 const unexistingArtwork = 9999;
 const testUserEmail = 'test@example.com';
 
 beforeAll(async () => {
-  pool = mysql.createPool(DEFAULT_POOL_CONFIG);
   // start transaction to be rolled back once tests end
-  await pool.query('START TRANSACTION');
+  await pool.execute('START TRANSACTION');
   // creates test user
-  await pool.query(`
+  await pool.execute(`
     INSERT INTO user (email, password)
     VALUES ('${testUserEmail}', 'password');
-  `)
+  `);
 
-  const testUserQuery = await pool.query(`
+  const [testUserQuery] = await pool.query(`
     SELECT id FROM user
     WHERE email = '${testUserEmail}'
     LIMIT 1
@@ -28,31 +26,31 @@ beforeAll(async () => {
 
 afterAll(async () => {
   // Roll back the transaction to undo any changes made during the tests
-  await pool.query('ROLLBACK');
+  await pool.execute('ROLLBACK');
   pool.end();
 });
 
 test('createArtwork creates an artwork and associates it with a user', async () => {
   await createArtwork(testUserId, existingArtwork);
 
-  const [rows] = await pool.query('SELECT * FROM artwork WHERE user_id = 1');
+  const [rows] = await pool.query('SELECT * FROM artwork WHERE user_id = ?', [testUserId]);
   const rowDataPackets = rows as mysql.RowDataPacket[];
 
   expect(rows).toHaveLength(1);
-  expect(rowDataPackets[0]).toMatchObject({ user_id: 1, external_id: 101 });
+  expect(rowDataPackets[0]).toMatchObject({ user_id: testUserId, external_id: existingArtwork });
 });
 
 
 describe('getUserEmailByArtworkId', () => {
-    it('returns the correct user email for an artwork', async () => {
-        await createArtwork(testUserId, existingArtwork);
-        const email = await getUserEmailByArtworkId(existingArtwork);
-        expect(email).toBe(testUserEmail);
-    });
+  it('returns the correct user email for an artwork', async () => {
+    await createArtwork(testUserId, existingArtwork);
+    const email = await getUserEmailByArtworkId(existingArtwork);
+    expect(email).toBe(testUserEmail);
+  });
 
-    it('returns null for an artwork with no user', async () => {
-        // Assuming we have no artwork with external_id of 9999
-        const email = await getUserEmailByArtworkId(unexistingArtwork);
-        expect(email).toBe(null);
-    });
+  it('returns null for an artwork with no user', async () => {
+    // Assuming we have no artwork with external_id of 9999
+    const email = await getUserEmailByArtworkId(unexistingArtwork);
+    expect(email).toBe(null);
+  });
 });
--- src/services/dbService.ts
+++ src/services/dbService.ts
@@ -1,14 +1,11 @@
 import mysql from 'mysql2/promise';
 import { DEFAULT_POOL_CONFIG } from '../config';
 
-
-const pool = mysql.createPool(DEFAULT_POOL_CONFIG);
+export const pool = mysql.createPool(DEFAULT_POOL_CONFIG);
 
 export const createArtwork = async (userId: number, externalId: number): Promise<void> => {
   try {
-    const [rows] = await pool.execute('SELECT * FROM user WHERE id = ? LIMIT 1', [userId]);
-
-    // .length only available as mysql.RowDataPacket[] interface
+    const [rows] = await pool.query('SELECT * FROM user WHERE id = ? LIMIT 1', [userId]);
     const rowDataPackets = rows as mysql.RowDataPacket[];
 
     if (rowDataPackets.length === 0) {
@@ -24,18 +21,18 @@ export const createArtwork = async (userId: number, externalId: number): Promise
 export async function getUserEmailByArtworkId(externalId: number): Promise<string | null> {
   const query = 'SELECT user.email FROM artwork JOIN user ON artwork.user_id = user.id WHERE artwork.external_id = ?';
   const params = [externalId];
+
   try {
-      const rows = await pool.query(query, params);
-      // .length only available as mysql.RowDataPacket[] interface
-      const rowDataPackets = rows as mysql.RowDataPacket[];
-      if (rowDataPackets.length > 0) {
-          return rowDataPackets[0].email;
-      } else {
-          return null;
-      }
+    const [rows] = await pool.query(query, params);
+    const rowDataPackets = rows as mysql.RowDataPacket[];
+
+    if (rowDataPackets.length > 0) {
+      return rowDataPackets[0].email;
+    } else {
+      return null;
+    }
   } catch (error) {
-      console.error('Error retrieving user email:', error);
-      throw error;
+    console.error('Error retrieving user email:', error);
+    throw error;
   }
 }
-
