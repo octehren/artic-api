badfbb738d20b45b6ebdcccfa39c56278610302d
octehren <108275018+octehren@users.noreply.github.com>
2023-05-24 01:58:30 +0200

    added login route

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,12 @@ import * as http from 'http';
 import * as url from 'url';
 
 /* begin custom services */
-//import { authenticateRequest } from './services/authService';
+import { authenticateRequest, getAuthForUser } from './services/authService';
 import apiService, { getArtworks, getArtwork } from './services/apiService';
 /* endof custom services */
 
-
-// Reference for auth; work on it later.
-// const server = http.createServer((req, res) => {
-//   const { pathname } = url.parse(req.url || '', true);
-
-//   if (pathname === '/protected') {
-//     if (authenticateRequest(req)) {
-//       res.writeHead(200, { 'Content-Type': 'text/plain' });
-//       res.end('Welcome to the protected route.');
-//     } else {
-//       res.writeHead(401, { 'Content-Type': 'text/plain' });
-//       res.end('Unauthorized.');
-//     }
-//   } else if (pathname === '/public') {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.end('Welcome to the public route.');
-//   } else {
-//     res.writeHead(404, { 'Content-Type': 'text/plain' });
-//     res.end('Not found.');
-//   }
-// });
-
 const server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse> = http.createServer(async (req, res) => {
   const requestUrl : url.UrlWithParsedQuery = url.parse(req.url as string, true);
-  
   switch(requestUrl.pathname) {
     case '/artworks':
       const page = requestUrl.query.page ? Number(requestUrl.query.page) : 1;
@@ -50,17 +27,49 @@ const server: http.Server<typeof http.IncomingMessage, typeof http.ServerRespons
         res.end(JSON.stringify(artwork, null, 2)); // prettifies response
       }
       break;
-    case '/dbtest':
-      res.writeHead(404, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ response: 'huhuhhdushdushduu' }));
-    default:
-      res.writeHead(404, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: 'Route not found' }));
-  }
-});
+      case '/login':
+        if (req.method === 'POST') {
+          try {
+            // accepts e-mail and password only on request body
+            const { email, password } = JSON.parse(await getRequestBody(req));
+            // performs authentication and gets the authorization token
+            const authToken = await getAuthForUser(email, password);
+            // returns the authorization token in response
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ token: authToken }));
+          } catch (error) {
+            console.error('Error during login:', error);
+            res.writeHead(500, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ error: 'Internal server error' }));
+          }
+        } else { // non POST req
+          res.writeHead(405, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'Method not allowed' }));
+        }
+        break;
+      default:
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Route not found' }));
+    }
+  });
 
 const port = process.env.PORT || "3000";
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+async function getRequestBody(req: http.IncomingMessage): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      resolve(body);
+    });
+    req.on('error', (error) => {
+      reject(error);
+    });
+  });
+}
